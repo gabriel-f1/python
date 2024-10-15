f2629ed26cd077c0636c8e3cdd7a6c633585593a
gabriel-f1 <162989980+gabriel-f1@users.noreply.github.com>
2024-10-15 12:00:07 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+
+print("<-------------------------------------------->\n|                                           |\n| CADASTRO DE DADOS DO USUÁRIO              |\n|                                           |\n<-------------------------------------------->\n")
+nome = input("Digite o seu nome:\n")
+idade = input("Informe a sua idade:\n") 
+cpf = input("Digite o seu CPF:\n")
+endereco = input("Informe o seu endereço:\n")
+
+print("------> CADASTRO REALIZADO! INFORMAÇÕES:\n\nNOME:", nome, "\nIDADE:", idade, "\nCPF:", cpf, "\nENDEREÇO:", endereco)
